e346faebe91bf184eb58f87f053f6996f681a87f
DJ <darienmartaze8@gmail.com>
2021-06-14 18:30:39 -0400

    QuestCraft Bot V2

---
+++ help.js
@@ -0,0 +1,16 @@
+module.exports = {
+  'help': {
+    description: 'Shows the list of commands or help on specified command.',
+    format: 'help [command-name]'
+  },
+  'ping': {
+    description: 'Checks connectivity with discord\'s servers.',
+    format: 'ping'
+  },
+  'say': {
+    aliases: ['repeat'],
+    description: 'Repeats whatever is said.',
+    format: 'say <message>'
+  }
+}
+
